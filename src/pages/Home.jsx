0168e97de56e627d28f57d546a901513fb471aaa
Cpvasanth <cpvasanthakumar@gmail.com>
2024-12-16 22:11:43 +0530

    `Added meta viewport tag to index.html and updated Home component with new styles and links`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
     return (
-        <>
-        <h1>this is home page</h1>
-        </>
-    )
+        <div className={styles.home}>
+            <div className={styles.container}>
+                <h1 className={styles.name}>Vasanthakumar C</h1>
+                <h2 className={styles.job}>Blockchain Developer</h2>
+                <div className={styles.buttons}>
+                    <Link to="/project" className={styles.worksBtn}>
+                        View Work
+                    </Link>
+                    <Link to="/contact" className={styles.contactBtn}>
+                        Contact Me
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
